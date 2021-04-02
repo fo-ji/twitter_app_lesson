5f519bff03e5ea28a2a327c2e53b5f63887f9077
fo-ji <57491651+fo-ji@users.noreply.github.com>
2021-04-02 16:43:18 +0900

    set auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
-import './App.css'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { login, logout, selectUser } from './features/userSlice'
+import { auth } from './firebase'
 
-function App() {
+import styles from './App.module.css'
+
+const App: React.FC = () => {
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    // userがログインやログアウトを実行した変化をキャッチする
+    const unSub = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photoUrl: authUser.photoURL,
+            displayName: authUser.displayName,
+          })
+        )
+      } else {
+        dispatch(logout())
+      }
+    })
+    return () => {
+      unSub()
+    }
+  }, [dispatch])
+
   return <div className="App"></div>
 }
 
