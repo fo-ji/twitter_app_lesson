cc930d5625795befbbdd8c875f3908a0baa5b643
fo-ji <57491651+fo-ji@users.noreply.github.com>
2021-04-03 17:46:54 +0900

    add send tweet

--- src/components/TweetInput.tsx
+++ src/components/TweetInput.tsx
@@ -1,14 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { selectUser } from '../features/userSlice'
-import { auth } from '../firebase'
+import firebase from 'firebase/app'
+import { auth, db, storage } from '../firebase'
 
-import { Avatar } from '@material-ui/core'
+import { Avatar, Button, IconButton } from '@material-ui/core'
+import AddAPhotoIcon from '@material-ui/icons/AddAPhoto'
 
 import styles from './TweetInput.module.css'
 
 const TweetInput = () => {
   const user = useSelector(selectUser)
+  const [tweetMsg, setTweetMsg] = useState('')
+  const [tweetImage, setTweetImage] = useState<File | null>(null)
+
+  const onChangeImageHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Non-null Assertion Operator
+    if (e.target.files![0]) {
+      setTweetImage(e.target.files![0])
+      e.target.value = ''
+    }
+  }
+
+  // 画像がある場合は、storageにアップロードしてから、urlを取得してdbに追加する
+  const sendTweet = (e: React.FormEvent<HTMLFormElement>) => {
+    // onSubmitでリロード回避
+    e.preventDefault()
+    if (tweetImage) {
+      const S = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+      const N = 16
+      const randomChar = Array.from(crypto.getRandomValues(new Uint32Array(N)))
+        .map((n) => S[n % S.length])
+        .join('')
+      const fileName = randomChar + '_' + tweetImage.name
+      const uploadTweetImage = storage.ref(`images/${fileName}`).put(tweetImage)
+      // storageに対して何らかの変化が起きた場合の後処理をonメソッドで実装する
+      uploadTweetImage.on(
+        firebase.storage.TaskEvent.STATE_CHANGED,
+
+        // 1. アップロードの進捗管理
+        () => {}, // 今回は未実装
+        // 2. エラーハンドリング
+        (error) => {
+          alert(error.message)
+        },
+        // 3. 正常終了した際の関数処理
+        async () => {
+          await storage
+            .ref('images')
+            .child(fileName)
+            .getDownloadURL()
+            .then(async (url) => {
+              await db.collection('posts').add({
+                avatar: user.photoUrl,
+                image: url,
+                text: tweetMsg,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                username: user.displayName,
+              })
+            })
+        }
+      )
+    } else {
+      db.collection('posts').add({
+        avatar: user.photoUrl,
+        image: '',
+        text: tweetMsg,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        username: user.displayName,
+      })
+    }
+    setTweetImage(null)
+    setTweetMsg('')
+  }
 
   return (
     <div>
